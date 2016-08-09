392aeaf06262b214671b9ada88c00051bfda9927
sinecostan <good.ryan@live.com>
2016-08-09 16:18:00 -0400

    added a lot to the mining skill (no javascript yet)
    also wrote design documents for skills, and taxonomy for how items should be classified in the .items object

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 //community drop rate guide for combat: http://services.runescape.com/m=rswiki/en/Community_-_Drop_Rate_Guide
-var hero_wc_xp = 0;
-var hero_wc_lvl = 1;
-var hero_fm_xp = 0;
-var hero_fm_lvl = 1;
-var firemakers = 0;
+
+// http://stackoverflow.com/questions/12612391/javascript-use-getelementbyid-to-populate-multiple-divs
+// http://stackoverflow.com/questions/11515383/why-is-element-innerhtml-bad-code
+// consider this to populate hero_location - getelementbyid may not be a good way to do this.
+
+//styling of the tabs begins here - eventually change this to a bunch of skill buttons from runescape
+
+function openskill(evt, skillname) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(skillname).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+//styling of the tabs ends here
+
 var xp_table = [0,83,174,276,388,512,650,801,969,1154,1562,2014,2512,3060,3668,4338,5076,5892,6792,7786,	8882,	10700,	12701,	14912,	17354,	20048,	23018,	26300,	29921,	33917,	38327,	43196,	50360,	58268,	66996,	76632,	87264,	99004,	111964,	126268,	142056,	159488,	178728,	205278,	234593,	266948,	302668,	342103,	385638,	433698,	486758,	545333,	610003,	681393,	775977,	880401,	995685,	1122957,	1263477,	1418613,	1589889,	1778985,	1987755,	2218251,	2472729,	2800511,	3162404,	3561957,	4003090,	4490122,	5027848,	5621532,	6276998,	7000679,	7799680,	8681834,	9794938,	11023890,	12380754,	13878834,	15532834,	17358986,	19375202,	21601274,	24059042,	26772626,	29768642,	33489980,	37598651,	42134984,	47143475,	52673273,	58778657,	65519522,	72962018,	81179189,	90251657,	100268450,	112556700,	126123990,	141103480,	157642150,	175902310,	196063120,	218322450,	242898710,270033080,299991820,333068930,373241007,417594547,466564787,520632295,580327634,646236609,719005976,799349844,888056638,985996843,1094131573];
 
 var log_types = {
 	logs:{
 	name:'logs',
-	cutters:0,
 	total:0,
 	level:1,
 	price:97,
@@ -18,7 +42,6 @@ var log_types = {
 	},
 	oak:{
 	name:'oak logs',
-	cutters:0,
 	total:0,
 	level:15,
 	price:77,
@@ -176,9 +199,176 @@ var hatchet_types = {
 	},
 };
 
+var ore_types = {
+	copper:{
+	name:'Copper Ore',
+	total:0,
+	level:1,
+	price:45,
+	mining_experience:17.5
+	},
+	tin:{
+	name:'Tin Ore',
+	total:0,
+	level:1,
+	price:65,
+	mining_experience:17.5
+	},
+	iron:{
+	name:'Iron Ore',
+	total:0,
+	level:15,
+	price:329,
+	mining_experience:35
+	},
+	silver:{
+	name:'Silver Ore',
+	total:0,
+	level:20,
+	price:67,
+	mining_experience:40
+	},
+	coal:{
+	name:'Coal',
+	total:0,
+	level:30,
+	price:187,
+	mining_experience:50
+	},
+	gold:{
+	name:'Gold Ore',
+	total:0,
+	level:40,
+	price:144,
+	mining_experience:65
+	},
+	mithril:{
+	name:'Mithril Ore',
+	total:0,
+	level:55,
+	price:207,
+	mining_experience:80
+	},
+	adamantite:{
+	name:'Adamantite Ore',
+	total:0,
+	level:70,
+	price:1018,
+	mining_experience:95
+	},
+	runite:{
+	name:'Runite Ore',
+	total:0,
+	level:85,
+	price:11304,
+	mining_experience:125
+	},
+};
+
+var pickaxe_types = {
+	no:{
+	name:'no pickaxe',
+	total:1, //this means the pickaxe will always exit loops that require a pickaxe equipped
+	level:1,
+	price:1,
+	accuracy:0,
+	damage:1,
+	speed:5,
+	durability:1
+	},
+	bronze:{
+	name:'bronze pickaxe',
+	total:0,
+	level:1,
+	price:179,
+	accuracy:110,
+	damage:30,
+	speed:5,
+	durability:110
+	},
+	iron:{
+	name:'iron pickaxe',
+	total:0,
+	level:10,
+	price:279,
+	accuracy:202,
+	damage:61,
+	speed:5,
+	durability:202
+	},
+	steel:{
+	name:'steel pickaxe',
+	total:0,
+	level:20,
+	price:749,
+	accuracy:316,
+	damage:122,
+	speed:5,
+	durability:316
+	},
+	black:{
+	name:'black pickaxe',
+	total:0,
+	level:25,
+	price:822,
+	accuracy:381,
+	damage:147,
+	speed:5,
+	durability:381
+	},
+	mithril:{
+	name:'mithril pickaxe',
+	total:0,
+	level:30,
+	price:1252,
+	accuracy:454,
+	damage:183,
+	speed:5,
+	durability:454
+	},
+	adamant:{
+	name:'adamant pickaxe',
+	total:0,
+	level:40,
+	price:2219,
+	accuracy:628,
+	damage:245,
+	speed:5,
+	durability:628 
+	},
+	rune:{
+	name:'rune pickaxe',
+	total:0,
+	level:50,
+	price:7377,
+	accuracy:850,
+	damage:306,
+	speed:5,
+	durability:850
+	},
+	dragon:{
+	name:'dragon pickaxe',
+	total:0,
+	level:60,
+	price:1971989,
+	accuracy:1132,
+	damage:367,
+	speed:5,
+	durability:1132
+	},
+};
+
+var hero_wc_xp = 0;
+var hero_wc_lvl = 1;
 var woodcutters = [0,0,0,0,0,0,0,0,0,0]; //number of woodcutters @ idle, tree, oak, willow, teak, maple, mahogany, yew, magic, elder
 var treenames = ["idle","logs","oak","willow","teak","maple","mahogany","yew","magic","elder"]; //tree names, the first entry is idle workers, then trees, then oak trees. following the variable convention, logs, oak_logs will create the right variable names
-var metaltype = ["no","bronze","iron","steel","black","mithril","adamant","rune","dragon"] //hatchet metal types
+var hatchet_metaltype = ["no","bronze","iron","steel","black","mithril","adamant","rune","dragon"] //hatchet metal types
+
+var hero_mining_xp = 0;
+var hero_mining_lvl = 1;
+var miners = [0,0,0,0,0,0,0,0,0,0]; //number of woodcutters @ idle, tin, copper, iron, silver, coal, gold, mithril, adamantite, runite
+var rocknames = ["idle","tin", "copper", "iron", "silver", "coal", "gold", "mithril", "adamantite", "runite"]; //tree names, the first entry is idle workers, then trees, then oak trees. following the variable convention, logs, oak_logs will create the right variable names
+var pickaxe_metaltype = ["no","bronze","iron","steel","mithril","adamant","rune","dragon"] //pickaxe metal types
 
 var hero_location = ["idle",0];
 	document.getElementById("hero_location").innerHTML = hero_location[0]; //display hero's location
@@ -195,12 +385,31 @@ for (i = 0; i <= 9; i++){
 	document.getElementById(locations).innerHTML = woodcutters[i];
 }
 
+//loop to initialize display of all ore types and all miner locations
+for (i = 0; i <= 9; i++){
+	if (i > 0){ //there are no "idle ore" or "no pickaxe" to display
+		var oretype = rocknames[i].concat("_ore.total");
+		document.getElementById(oretype).innerHTML = ore_types[rocknames[i]]["total"];
+	}
+	//var locations = treenames[i]; ore rocks not yet programmed
+	//locations = locations.concat("_ore_miners");
+	//document.getElementById(locations).innerHTML = woodcutters[i];
+}
+
 for (i = 1; i <= 8; i++){ //loop to give the player some hatchets for DEBUGGING PURPOSES!!!!!!
-	hatchet_types[metaltype[i]]["total"] = 5;
-	var logtype = metaltype[i].concat("_hatchet.total");
-	document.getElementById(logtype).innerHTML = hatchet_types[metaltype[i]]["total"];
-	logtype = metaltype[i].concat("_hatchet.durability");
-	document.getElementById(logtype).innerHTML = hatchet_types[metaltype[i]]["durability"];
+	hatchet_types[hatchet_metaltype[i]]["total"] = 5;
+	var logtype = hatchet_metaltype[i].concat("_hatchet.total");
+	document.getElementById(logtype).innerHTML = hatchet_types[hatchet_metaltype[i]]["total"];
+	logtype = hatchet_metaltype[i].concat("_hatchet.durability");
+	document.getElementById(logtype).innerHTML = hatchet_types[hatchet_metaltype[i]]["durability"];
+}
+
+for (i = 1; i <= 7; i++){ //loop to give the player some pickaxes for DEBUGGING PURPOSES!!!!!!
+	pickaxe_types[pickaxe_metaltype[i]]["total"] = 5;
+	var logtype = pickaxe_metaltype[i].concat("_pickaxe.total");
+	document.getElementById(logtype).innerHTML = pickaxe_types[pickaxe_metaltype[i]]["total"];
+	logtype = pickaxe_metaltype[i].concat("_pickaxe.durability");
+	document.getElementById(logtype).innerHTML = pickaxe_types[pickaxe_metaltype[i]]["durability"];
 }
 
 	document.getElementById("current_hatchet.name").innerHTML = hatchet_types[current_hatchet]["name"]; //display hero's equipped hatchet
@@ -275,8 +484,8 @@ function cut_trees(){
 							document.getElementById(hatchet_type).innerHTML = hatchet_types[current_hatchet]["total"] //display number of current hatchets
 							if (hatchet_types[current_hatchet]["total"] === 0){ //unequip the hatchet if there are none left
 								for (k = 0; k <= 8; k++) { //loop that equips the highest possible level hatchet if the last current one breaks
-									if (hatchet_types[metaltype[k]]["total"] > 0 && hero_wc_lvl >= hatchet_types[metaltype[k]]["level"]) { //makes sure there is a spare hatchet that the hero is high enough to use
-										current_hatchet = metaltype[k]; //equip new hatchet
+									if (hatchet_types[hatchet_metaltype[k]]["total"] > 0 && hero_wc_lvl >= hatchet_types[hatchet_metaltype[k]]["level"]) { //makes sure there is a spare hatchet that the hero is high enough to use
+										current_hatchet = hatchet_metaltype[k]; //equip new hatchet
 									}
 								}
 								document.getElementById("current_hatchet.name").innerHTML = hatchet_types[current_hatchet]["name"];
@@ -342,8 +551,8 @@ function herotree(){
 						document.getElementById(hatchet_type).innerHTML = hatchet_types[current_hatchet]["total"] //display number of current hatchets
 						if (hatchet_types[current_hatchet]["total"] === 0){ //unequip the hatchet if there are none left
 							for (k = 0; k <= 8; k++) { //loop that equips the highest possible level hatchet if the last current one breaks
-								if (hatchet_types[metaltype[k]]["total"] > 0 && hero_wc_lvl >= hatchet_types[metaltype[k]]["level"]) { //makes sure there is a spare hatchet that the hero is high enough to use
-									current_hatchet = metaltype[k]; //equip new hatchet
+								if (hatchet_types[hatchet_metaltype[k]]["total"] > 0 && hero_wc_lvl >= hatchet_types[hatchet_metaltype[k]]["level"]) { //makes sure there is a spare hatchet that the hero is high enough to use
+									current_hatchet = hatchet_metaltype[k]; //equip new hatchet
 								}
 							}
 							document.getElementById("current_hatchet.name").innerHTML = hatchet_types[current_hatchet]["name"];
@@ -385,5 +594,5 @@ window.setInterval(function(){
 	herotree();
 	cut_trees();
 	
-}, 600);
+}, 30);
 
